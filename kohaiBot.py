2c2e4a042240efbbc5d0d3b052a77e59c13ad715
Devon Graves <dhg5054@gmail.com>
2017-11-14 11:03:32 -0500

    Add files via upload

--- kohaiBot.py
+++ kohaiBot.py
@@ -1,10 +1,16 @@
-import requests
+from flask import requests, Flask
 
-group_id = 35125865
 URL = "https://api.groupme.com/v3/bots/post"
 accessToken = "LWjIXZM4dl5CrDK8zzhZN4w7XUDTd2fcHhqfSLA8"
 bot_id = "0762588ce49d56ec028df8dafe"
-text = "I'm still learning how to communicate! Bear with me for a moment!"
-payload = {"bot_id" : bot_id, "text" : text}
+test_id = "0757dcd554eef85ed5096a34a5"
+text = "Hope you all have a great day today!"
+payload = {"bot_id" : test_id, "text" : text}
 
 r = requests.post(URL, data=payload)
+
+app = Flask(_name_)
+@app.route('/', methods='[POST'])
+def result():
+	print(request.form['name'])
+	return 'Received!'
